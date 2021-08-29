b52ac155ff8c1f1e2e7c782e8c090c39729724c6
mwezn <m.w.g.nelson@gmail.com>
2021-08-29 17:49:40 +0100

    More changes

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -7,9 +7,10 @@ function Section1() {
 
       <div class="intro"> 
       <h1>Jones and Palmer</h1>
-      <h2>I built this webpage for a design agency (Jones&Palmer) based in Birmingham
-      based on this template they had given me.
-      
+      <h2>I built this webpage for a Birmingham design agency (Jones&Palmer) 
+      based on <a target='_blank' href='../assets/website.pdf'>this template</a> they had given me.
+      This is my second attempt since the first attempt I tried located <a target='_blank' href='https://JonesPalmer.mwezn.repl.co'> at this url</a>
+      was poorly desgined. 
       I made one major mistake by not designing it with mobile first principles! Hence why it doesnt scale images & font-sizes correctly</h2>
 
     <h3>Eprae volorepudio bea cus magniste is dipsunt res pratur, nectur molorro vitaquo molore et eum con rem nonseque ium
